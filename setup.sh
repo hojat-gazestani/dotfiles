c2ccf68e539da89e84f7a70d66e58a382b78e42c
hojat-gazestani <hojatgazestani@gmail.com>
2024-08-19 03:10:54 +0330

    remove zshrc if exist

--- setup.sh
+++ setup.sh
@@ -8,5 +8,8 @@ mkdir -p ~/.config
 # Link Git config if it doesn’t exist
 [ ! -e ~/.config/git ] && ln -s "$PWD/config/git" ~/.config/git
 [ ! -e ~/.config/starship.toml ] && ln -s "$PWD/config/starship.toml" ~/.config/starship.toml
+if [ -f ~/.zshrc ] && [ ! -L ~/.zshrc ]; then
+  rm ~/.zshrc
+fi
 [ ! -e ~/.config/.zshrc ] && ln -s "$PWD/config/.zshrc" ~/.zshrc
 [ ! -e ~/.config/ripgreprc ] && ln -s "$PWD/config/ripgreprc" ~/.config/ripgreprc
