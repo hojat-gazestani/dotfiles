a9759094fea165d380d1f5a039cfa5cd47b9afb3
Hojat Gazestani <39824268+hojat-gazestani@users.noreply.github.com>
2026-06-17 12:08:13 +0330

    fix - codecompanion issue

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -1,103 +1,5 @@
 require("lazy").setup({
 
-  -- code companion
-  "olimorris/codecompanion.nvim",
-  dependencies = {
-    "ravitemer/mcphub.nvim",
-    "nvim-lua/plenary.nvim",
-    "nvim-treesitter/nvim-treesitter",
-  },
-                
-
-  require("codecompanion").setup({
-      adapters = {
-          http = {
-          	qwen = function()
-      	    return require("codecompanion.adapters").extend(
-      		"openai_compatible",
-      		{
-                  	    env = {
-      	    	        url = "http://saca-api.inside.sahab.ir:3100/v1",
-      	    	        api_key = "sk-0EdNANfss1LiE5caKcCboQ",
-                  	        chat_url = "/v1/chat/completions",
-                  	        models_endpoint = "/v1/models",
-                  	    },
-              	}
-          	    )
-          	end,
-          },
-  	},
-
-  	strategies = {
-  	    chat = { adapter = "qwen" },
-  	    inline = { adapter = "qwen" },
-  	    cmd = { adapter = "qwen" },
-  	},
-  },
-
-    --opts = {
-    --    -- Main model
-    --    interactions = {
-    --      chat = {
-    --        adapter = "openai",
-    --        model = "AliBaba/Qwen3.6-27B",
-    --      },
-    --      inline = {
-    --        adapter = "openai",
-    --        model = "AliBaba/Qwen3.6-27B",
-    --      },
-    --      cmd = {
-    --        adapter = "openai",
-    --        model = "AliBaba/Qwen3.6-27B",
-    --      },
-    --      background = {
-    --        adapter = "openai",
-    --        model = "AliBaba/Qwen3.6-27B",
-    --      },
-    --    },
-    --    -- Adapter configuration
-    --    adapters = {
-    --      http = {
-    --        openai = function()
-    --          return require("codecompanion.adapters").extend("openai", {
-    --            env = {
-    --              api_key = "sk-0EdNANfss1LiE5caKcCboQ",
-    --              url = "http://saca-api.inside.sahab.ir:3100/v1", 
-    --            },
-    --            -- Specific parameters for Qwen
-    --            schema = {
-    --              model = {
-    --                default = "AliBaba/Qwen3.6-27B",
-    --              },
-    --              parameters = {
-    --                -- Qwen-specific settings
-    --                temperature = 0.7,
-    --                max_tokens = 4096,
-    --              },
-    --            },
-    --          })
-    --        end,
-    --      },
-    --    },
-    --    opts = {
-    --      log_level = "DEBUG", 
-    --      -- Optional: Configure how CodeCompanion interacts with your API
-    --      api = {
-    --        timeout = 60, -- Seconds
-    --        --retry = {
-    --        --  attempts = 3,
-    --        --  delay = 2,
-    --        --},
-    --      },
-    --    },
-    --    -- Strategy for how CodeCompanion handles the model
-    --    strategy = "chat", -- or "agent" for more complex tasks
-    --  },
-    --  config = function(_, opts)
-    --    require("codecompanion").setup(opts)
-    --  end,
-    --},
-
   {
     "MeanderingProgrammer/render-markdown.nvim",
     ft = { "markdown", "codecompanion" }
@@ -157,16 +59,7 @@ require("lazy").setup({
     lazy = false,
     build = ':TSUpdate'
   },
-  --  code compoanion
-  {
-    "olimorris/codecompanion.nvim",
-    version = "^19.0.0",
-    opts = {},
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-    },
-  },
+
   --  LSP support
   {
     "neovim/nvim-lspconfig",
@@ -209,4 +102,37 @@ require("lazy").setup({
   "tpope/vim-surround",
   "tpope/vim-fugitive",
 
+   --  code compoanion
+  {
+    "olimorris/codecompanion.nvim",
+    version = "^19.0.0",
+    opts = {
+      adapters = {
+        http = {
+          qwen = function()
+            return require("codecompanion.adapters").extend(
+              "openai_compatible",
+              {
+                env = {
+                  url = "http://saca-api.inside.sahab.ir:3100",
+                  api_key = "sk-0EdNANfss1LiE5caKcCboQ",
+                },
+              }
+            )
+          end,
+        },
+      },
+
+      strategies = {
+        chat = { adapter = "qwen" },
+        inline = { adapter = "qwen" },
+        cmd = { adapter = "qwen" },
+      },
+    },
+    dependencies = {
+      "ravitemer/mcphub.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+  }, 
 })
